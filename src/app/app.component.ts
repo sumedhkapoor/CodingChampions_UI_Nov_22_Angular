25e86db2a4e05c550a597aaddf3911c05c6b5fda
Sumedh Kapoor <sumedhk8@gmail.com>
2022-11-24 11:18:18 +0530

    Starter project for coding challenge

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CodingChampions_UI_Nov_22_Angular';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TicTacToeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
